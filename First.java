6da99c3ac591a2322738d5a9f720ad24f405dc95
prakash <vd.prakash009@gmail.com>
2023-03-16 16:12:41 +0530

    Added new file and change existing file

--- First.java
+++ First.java
@@ -1,14 +1,7 @@
-class First{
+public class First {
 
-public static void main(String args[])
-{
+	public static void main(String args[]) {
 
-if(true){
-
-System.out.print("First github");
-}
-else{
-System.out.print("Hello world");
-}
-}
+		System.out.print("First github");
+	}
 }
